1cee0727d27a990f1b57384829ad06c3a0d55089
Lazy Goose <shamal.ma.rss@gmail.com>
2024-07-28 17:01:07 +0300

    feat: init typeorm with DatabaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,16 @@ import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
 
+import { DatabaseModule } from './database/database.module';
+
 @Module({
   imports: [
+    DatabaseModule,
     AuthModule,
     CartModule,
-    OrderModule,
+    OrderModule
   ],
-  controllers: [
-    AppController,
-  ],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
