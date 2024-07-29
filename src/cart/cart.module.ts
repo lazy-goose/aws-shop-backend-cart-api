b1cbe12e83d4cc350a68a5adfd72dbb7b1bd2d8d
Lazy Goose <shamal.ma.rss@gmail.com>
2024-07-29 11:39:46 +0300

    fix: import CartItemEntity in CartModule

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -4,10 +4,11 @@ import { CartEntity } from 'src/database/entities/cart.entity';
 import { OrderModule } from '../order/order.module';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { CartItemEntity } from 'src/database/entities/cart-item.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([CartEntity]),
+    TypeOrmModule.forFeature([CartEntity, CartItemEntity]),
     forwardRef(() => OrderModule),
   ],
   providers: [CartService],
