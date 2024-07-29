ead5d3ba221a095e1a6953bcf9514247abfc3f1b
Lazy Goose <shamal.ma.rss@gmail.com>
2024-07-29 06:05:33 +0300

    fix: import UsersModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,18 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
-
 import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
-
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     DatabaseModule,
     AuthModule,
     CartModule,
-    OrderModule
+    OrderModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [],
